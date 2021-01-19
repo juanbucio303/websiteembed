53fedc2b792dffbe353a9d7a99d3b7aa6799ece6
juanbucio303 <juanbucio303@gmail.com>
2021-01-19 11:44:51 -0600

    Fisrt commit

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  var DEFAULT_URL = 'https://www.google.co.in/maps/search/pubs/';
+  return <WebView source={{ uri: 'https://www.google.com/' }} />;
 }
 
 const styles = StyleSheet.create({
